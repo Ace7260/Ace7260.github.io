dcbe21c3a6f5f2ad9ece2aa9cf79f423e5138062
Ace7260 <irano7260@gmail.com>
2022-09-24 08:38:25 +0200

    adjusting some style ui

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const dark = document.querySelector('.dark');
 const light = document.getElementsByTagName('nav');
+const cont = document.querySelector('.content');
 dark.addEventListener('click', (event) => {
     alert("hello");
 
+    cont.classList.add('.dog');
 })
