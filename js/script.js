2eeb36cc85459e922a87284ad5daa5976990f86e
EddyNobel21 <enirakoze@asystresources.com>
2023-10-09 16:34:40 +0200

    adding version

--- js/script.js
+++ js/script.js
@@ -6,3 +6,10 @@ dark.addEventListener('click', (event) => {
 
     cont.classList.add('.dog');
 })
+const selectTag = document.querySelector('#version');
+selectTag.addEventListener('change', function() {
+  const selectedOption = selectTag.options[selectTag.selectedIndex];
+  const redirectUrl = selectedOption.value;
+
+  window.location.href = redirectUrl;
+});
